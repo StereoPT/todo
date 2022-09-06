48f3d331e18269793d84d2c4df08eae433fd90e8
Guido Pereira <stereopt@gmail.com>
2022-09-06 10:55:23 +0100

    Display Tasks

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,29 @@
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <div class="flex justify-center items-center min-h-screen">
+    <div class="bg-white p-8 rounded-lg">
+      <h1 class="text-3xl font-bold">ToDo</h1>
+      <ul>
+        <li v-for="(task, index) in tasks" :key="index">{{ task }}</li>
+      </ul>
+    </div>
+  </div>
 </template>
 
-<script></script>
+<script>
+export default {
+  name: 'ToDo',
+  data() {
+    return {
+      tasks: [
+        "Task number one",
+        "Task number two",
+        "Another task",
+        "Yet another task"
+      ]
+    }
+  }
+}
+</script>
 
-<style scoped> </style>
+<style scoped>
+</style>
